ac256384643a475c547fed4aabf95ffc4b289bae
mellmers <moritz.ellmers@web.de>
2019-07-31 01:31:31 +0200

    bugfix

--- src/api/utils/database.js
+++ src/api/utils/database.js
@@ -3,7 +3,7 @@ const u = process.env.DB_USERNAME;
 const p = process.env.DB_PASSWORD;
 let login = '';
 if (u && p) {
-    login = u + ':' + p;
+    login = u + ':' + p + '@';
 }
 mongoose.connect('mongodb://' + login + 'localhost/' + (process.env.DB_NAME || 'healthapp'), {useNewUrlParser: true});
 mongoose.Promise = global.Promise;
